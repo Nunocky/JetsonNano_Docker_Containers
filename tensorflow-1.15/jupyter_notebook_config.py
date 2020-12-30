8d82c993d25b409ac505915b0abdb80e6991900c
NUNOKAWA Masato <masato.nunokawa@gmail.com>
2020-12-30 13:44:58 +0900

    initial commit

---
+++ tensorflow-1.15/jupyter_notebook_config.py
@@ -0,0 +1,6 @@
+c.NotebookApp.notebook_dir = '/tf'
+c.NotebookApp.ip = '*'
+c.NotebookApp.open_browser = False
+c.NotebookApp.port = 8888
+c.NotebookApp.password = u'argon2:$argon2id$v=19$m=10240,t=10,p=8$kDrfziXaxPvbrLZfP2oTKA$zSNR85lqmziWEVjx/ApauQ'    
+
